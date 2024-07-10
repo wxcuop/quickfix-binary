a9a26ccc5f20110902c95be71c14fd5266a7b09d
wxcuop <94150159+wxcuop@users.noreply.github.com>
2024-07-10 00:16:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ import glob
 import sys
 import sysconfig
 from distutils.command.build_ext import build_ext
-
 from setuptools import setup, Extension
-
+from datetime import datetime, timezone
 
 class build_ext_subclass(build_ext):
     def build_extensions(self):
@@ -35,20 +34,21 @@ class build_ext_subclass(build_ext):
 
         build_ext.build_extensions(self)
 
-
 # Remove the "-Wstrict-prototypes" compiler option, which isn't valid for C++.
-
 cfg_vars = sysconfig.get_config_vars()
 for key, value in cfg_vars.items():
     if type(value) == str:
         cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
+
+# Get the current UTC date
+utc_now = datetime.now(timezone.utc).strftime('%Y%m%d')
 
 long_description = ''
 with open('LICENSE') as file:
     license_ = file.read()
 
 setup(name='quickfix-binary',
-      version='1.15.1',
+      version=f'master-{utc_now}',
       py_modules=['quickfix', 'quickfixt11', 'quickfix40', 'quickfix41', 'quickfix42', 'quickfix43', 'quickfix44',
                   'quickfix50', 'quickfix50sp1', 'quickfix50sp2'],
       data_files=[('share/quickfix', glob.glob('spec/FIX*.xml'))],
