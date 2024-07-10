43affd7f59cf927990b122be562f17d8f1ff9b88
wxcuop <94150159+wxcuop@users.noreply.github.com>
2024-07-09 23:22:00 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,70 +1,32 @@
-name: Build and Deploy
+git clone --depth 1 https://github.com/quickfix/quickfix.git
 
-on:
-  workflow_dispatch:
+cd quickfix
 
-jobs:
-  build:
-    runs-on: ${{ matrix.os }}
-    strategy:
-      fail-fast: false
-      matrix:
-        python-version: [ '3.12' ]
-        os: [ 'ubuntu-latest' ]
-    steps:
-      - uses: actions/checkout@v4
-      - name: Setup
-        run: |
-          sudo apt-get update
-          sudo apt-get install -y build-essential autoconf automake libtool pkg-config openssl libssl-dev dos2unix libltdl-dev
-          sudo apt-get install -y mysql-server mysql-client libmysqlclient-dev
-          sudo apt-get install -y postgresql postgresql-contrib libpq-dev
-      - name: Setup python
-        uses: actions/setup-python@v4
-        with:
-          python-version: ${{ matrix.python-version }}
-      - name: Install dependencies
-        run: pip install setuptools wheel auditwheel
-      - name: Create C++ directory
-        run: mkdir -p C++
-      - name: Create dummy MySQLStubs.h
-        run: |
-          echo '#ifndef MYSQLSTUBS_H' > C++/MySQLStubs.h
-          echo '#define MYSQLSTUBS_H' >> C++/MySQLStubs.h
-          echo '// Dummy content' >> C++/MySQLStubs.h
-          echo '#endif // MYSQLSTUBS_H' >> C++/MySQLStubs.h
-      - name: Convert line endings
-        run: dos2unix ./build.sh
-      - name: Make build.sh executable
-        run: chmod +x ./build.sh
-      - name: Build
-        env:
-          CPLUS_INCLUDE_PATH: /usr/include/mysql
-        run: ./build.sh
-      - name: Fix linux wheel
-        run: auditwheel repair $(ls ./dist/*.whl | head -1) --plat manylinux_2_17_x86_64
+autoconf
 
-  deploy:
-    needs: build
-    permissions:
-      pages: write
-      id-token: write
+./bootstrap
 
-    environment:
-      name: github-pages
-      url: ${{ steps.deployment.outputs.page_url }}
+./configure --with-python3 --with-openssl && make && make check
 
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout
-        uses: actions/checkout@v4
-      - name: Copy wheel to dist
-        run: |
-          mkdir -p dist
-          cp $(ls ./dist/*.whl | head -1) dist/
-      - name: Deploy to GitHub Pages
-        id: deployment
-        uses: actions/deploy-pages@v4
-        with:
-          github_token: ${{ secrets.GH_PAT }}
-          publish_dir: ./dist
+cd ../
+
+mkdir ./C++
+mkdir ./spec
+
+cp quickfix/src/python3/*.py ./
+cp quickfix/src/C++/*.h ./C++
+cp quickfix/src/C++/*.hpp ./C++
+cp quickfix/src/C++/*.cpp ./C++
+cp -R quickfix/src/C++/double-conversion ./C++
+cp quickfix/src/python3/QuickfixPython.cpp ./C++
+cp quickfix/src/python3/QuickfixPython.h ./C++
+
+cp quickfix/LICENSE ./
+
+cp quickfix/spec/FIX*.xml ./spec
+
+
+touch ./C++/config.h
+touch ./C++/config_windows.h
+
+python setup.py bdist_wheel
